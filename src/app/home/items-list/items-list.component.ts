1a04b9e1e71d86b1a6755551cc8f4a63e5733f39
Touhidur Rahman <trforkan@gmail.com>
2022-09-28 19:29:33 +0600

    toggle added

--- src/app/home/items-list/items-list.component.ts
+++ src/app/home/items-list/items-list.component.ts
@@ -11,11 +11,11 @@ export class ItemsListComponent implements OnInit {
   @Input() itemName: string  = "Organization";
 
   itemList: any = [
-    { name: "List 0001", status: true },
-    { name: "List 0002", status: true },
-    { name: "List 0003", status: true },
-    { name: "List 0004", status: true },
-    { name: "List 0005", status: true },
+    { name: "List 0001", status: true, expanded: false },
+    { name: "List 0002", status: true, expanded: false },
+    { name: "List 0003", status: true, expanded: false },
+    { name: "List 0004", status: true, expanded: false },
+    { name: "List 0005", status: true, expanded: false },
   ];
 
 
@@ -27,6 +27,16 @@ export class ItemsListComponent implements OnInit {
   buttonTapped(index:number) {
     this.itemList[index].status = !this.itemList[index].status;
     console.log(this.itemList);
+  }
+
+  openExpansion(index: number) {
+  // console.log(index);
+    this.itemList[index]
+    for(let i=0; i<this.itemList.length; i++) {
+      if(i==index)this.itemList[i].expanded=true;
+      else this.itemList[i].expanded=false;
+    }
+    // console.log(this.itemList)
   }
 
 }
